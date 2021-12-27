1187d8737e16d884483fccd4cf64bf5d0d3f3707
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2021-12-27 04:23:26 +0300

    Create debug application

---
+++ Code/DebugApp/Debug.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Civil.ApplicationServices;
+using Autodesk.Civil.DatabaseServices;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.Civil.DataShortcuts;
+using System.Globalization;
+
+using Civil3D_CustomNodes;
+
+
+namespace DebugApp
+{
+    public class Debug
+    {
+        [CommandMethod("test1")]
+        public static void RunTest()
+        {
+            //Selection.SelectGeometryByRectangle("Водосток", 0.58497763, -0.3, 0.7, 1.1, "LINE", true); //0.63, 0.952
+            Selection.debug_only();
+        }
+    }
+}
