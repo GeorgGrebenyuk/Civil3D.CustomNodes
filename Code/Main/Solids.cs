a890fe6995f766faba93b313b0b7bc655d549dc8
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2021-12-31 18:03:30 +0300

    Update Solids.cs
    Change logical of identify faces (from string-centroids to position face at faces's collection and parent-solid)

--- Code/Main/Solids.cs
+++ Code/Main/Solids.cs
@@ -48,7 +48,7 @@ namespace Autodesk.Civil3D_CustomNodes
             }
         }
         [MultiReturn(new[] { "FacesId", "FacesCentroid" })]
-        public static Dictionary<string, object> GetSolid3dFacesCentroids (Autodesk.AutoCAD.DynamoNodes.Document doc_dyn, ObjectId solid_id, bool CreatePoints = false) //List <Dictionary<string, object>>
+        public static Dictionary<string, object> GetSolid3dFacesCentroids (Autodesk.AutoCAD.DynamoNodes.Document doc_dyn, List <ObjectId> solids_id_list, bool CreatePoints = false) //List <Dictionary<string, object>>
         {
 
             //Document doc = Application.DocumentManager.MdiActiveDocument;
@@ -70,76 +70,77 @@ namespace Autodesk.Civil3D_CustomNodes
                     //// Open the Block table record Model space for write
                     //BlockTableRecord acBlkTblRec;
                     //acBlkTblRec = tr.GetObject(acBlkTbl[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
-
-                    FullSubentityPath path = new FullSubentityPath(new ObjectId[1] { solid_id }, new SubentityId(SubentityType.Null, IntPtr.Zero));
-                    
-                    Autodesk.AutoCAD.DatabaseServices.Solid3d OneSolid = tr.GetObject(solid_id, OpenMode.ForRead) as Autodesk.AutoCAD.DatabaseServices.Solid3d;
-                    //Acad3DSolid OneSolid2 = (Acad3DSolid)OneSolid.AcadObject;
-                    using (Brep brep = new Brep(path))
+                    foreach (ObjectId OneSolidId in solids_id_list)
                     {
-                        foreach (Autodesk.AutoCAD.BoundaryRepresentation.Face face in brep.Faces)
+                        FullSubentityPath path = new FullSubentityPath(new ObjectId[1] { OneSolidId }, new SubentityId(SubentityType.Null, IntPtr.Zero));
+
+                        using (Brep brep = new Brep(path))
                         {
-                            LoopVertexCollection faces_vertexes = null;
-                            try
+                            List<Autodesk.AutoCAD.BoundaryRepresentation.Face> solid_faces = brep.Faces.ToList();
+                            for (int i1 = 0; i1 < solid_faces.Count; i1++)
                             {
-                                faces_vertexes = face.Loops.First().Vertices;
-                            }
-                            catch (Autodesk.AutoCAD.BoundaryRepresentation.Exception ex)
-                            {
-                                doc.Editor.WriteMessage("Error " + ex);
-                            }
-                            
-                            if (faces_vertexes!= null && faces_vertexes.Count() == 3)
-                            {
-                                List<Point3d> faces_points_temp = new List<Point3d>();
-
-                                foreach (Autodesk.AutoCAD.BoundaryRepresentation.Vertex OneFaceVertex in faces_vertexes)
+                                Autodesk.AutoCAD.BoundaryRepresentation.Face face = solid_faces[i1];
+                                string Face_Id = $"{OneSolidId.Handle}_{i1}";
+                                LoopVertexCollection faces_vertexes = null;
+                                try
                                 {
-                                    faces_points_temp.Add(OneFaceVertex.Point);
+                                    faces_vertexes = face.Loops.First().Vertices;
+                                }
+                                catch (Autodesk.AutoCAD.BoundaryRepresentation.Exception ex)
+                                {
+                                    doc.Editor.WriteMessage("Error " + ex);
                                 }
 
-                                List<Point3d> faces_points = new List<Point3d>();
-                                List<string> temp_points_str = new List<string>();
-                                foreach (Point3d OnePnt in faces_points_temp)
+                                if (faces_vertexes != null && faces_vertexes.Count() == 3)
                                 {
-                                    string temp_point_str = GetPointStringRepresentation(OnePnt, 4);
-                                    if (!temp_points_str.Contains(temp_point_str))
+                                    List<Point3d> faces_points_temp = new List<Point3d>();
+
+                                    foreach (Autodesk.AutoCAD.BoundaryRepresentation.Vertex OneFaceVertex in faces_vertexes)
                                     {
-                                        faces_points.Add(OnePnt);
-                                        temp_points_str.Add(temp_point_str);
-                                    }
-                                }
-                                if (faces_points.Count == 3)
-                                {
-                                    Point3d face_centroid = GetCentroidByFace(faces_points);
-                                    string centroid_str = GetPointStringRepresentation(face_centroid, 3);
-                                    if (!FacesId.Contains(centroid_str))
-                                    {
-                                        FacesId.Add(centroid_str);
-                                        FacesCentroid.Add(DynGeom.Point.ByCoordinates(face_centroid.X, face_centroid.Y, face_centroid.Z));
+                                        faces_points_temp.Add(OneFaceVertex.Point);
                                     }
 
+                                    List<Point3d> faces_points = new List<Point3d>();
+                                    List<string> temp_points_str = new List<string>();
+                                    foreach (Point3d OnePnt in faces_points_temp)
+                                    {
+                                        string temp_point_str = GetPointStringRepresentation(OnePnt, 4);
+                                        if (!temp_points_str.Contains(temp_point_str))
+                                        {
+                                            faces_points.Add(OnePnt);
+                                            temp_points_str.Add(temp_point_str);
+                                        }
+                                    }
+                                    if (faces_points.Count == 3)
+                                    {
+                                        Point3d face_centroid = GetCentroidByFace(faces_points);
+                                        string centroid_str = GetPointStringRepresentation(face_centroid, 3);
+                                        if (!FacesId.Contains(Face_Id))
+                                        {
+                                            FacesId.Add(Face_Id);
+                                            FacesCentroid.Add(DynGeom.Point.ByCoordinates(face_centroid.X, face_centroid.Y, face_centroid.Z));
+                                        }
+                                    }
                                 }
                             }
- 
-
                         }
-
-                        //Autodesk.Civil3D_CustomNodes.Geometry.SetAcadPointsStyle(0, 1);
-                    }
+                    }                  
                     tr.Commit();
                 }
             }
             FacesInfo.Add("FacesId", FacesId); FacesInfo.Add("FacesCentroid", FacesCentroid);
             return FacesInfo;
         }
+        /// <summary>
+        /// Struct for keep data about face in nethod SetMaterialByFacesCentroids
+        /// </summary>
         private struct FacesProps
         {
             public SubentityId id;
             public AutoCAD.Colors.Color color;
             public ObjectId material;
         }
-        public static void SetMaterialByFacesCentroids (Autodesk.AutoCAD.DynamoNodes.Document doc_dyn, ObjectId solid_id, Dictionary <string,string> faces_info, 
+        public static void SetMaterialByFacesCentroids (Autodesk.AutoCAD.DynamoNodes.Document doc_dyn, List<ObjectId> solids_id_list, List<Dictionary <string,string>> faces_info_list, 
             Dictionary <string, ObjectId> MaterialsByName, Dictionary<string, Autodesk.AutoCAD.Colors.Color> ColorsByNames, bool UseColors = false)
         {
             Document doc = doc_dyn.AcDocument;
@@ -150,93 +151,56 @@ namespace Autodesk.Civil3D_CustomNodes
             {
                 using (Transaction tr = db.TransactionManager.StartTransaction())
                 {
-                    FullSubentityPath path = new FullSubentityPath(new ObjectId[1] { solid_id }, new SubentityId(SubentityType.Null, IntPtr.Zero));
-                    Autodesk.AutoCAD.DatabaseServices.Solid3d OneSolid = tr.GetObject(solid_id, OpenMode.ForWrite) as Autodesk.AutoCAD.DatabaseServices.Solid3d;
-
-                    //Временные коллекции
-                    List<FacesProps> faces = new List<FacesProps>();
-                    using (Brep brep = new Brep(path))
+                    for (int i2 = 0; i2 < solids_id_list.Count; i2++)
                     {
-                        foreach (Autodesk.AutoCAD.BoundaryRepresentation.Face face in brep.Faces)
+                        ObjectId OneSolidId = solids_id_list[i2];
+                        Dictionary<string, string> faces_info = faces_info_list[i2];
+                        FullSubentityPath path = new FullSubentityPath(new ObjectId[1] { OneSolidId }, new SubentityId(SubentityType.Null, IntPtr.Zero));
+                        Autodesk.AutoCAD.DatabaseServices.Solid3d OneSolid = tr.GetObject(OneSolidId, OpenMode.ForWrite) as Autodesk.AutoCAD.DatabaseServices.Solid3d;
+
+                        //Временные коллекции
+                        List<FacesProps> faces = new List<FacesProps>();
+                        using (Brep brep = new Brep(path))
                         {
-                            LoopVertexCollection faces_vertexes = null;
-                            SubentityId face_id = face.SubentityPath.SubentId;
-                            try
+                            List<Autodesk.AutoCAD.BoundaryRepresentation.Face> solid_faces = brep.Faces.ToList();
+                            foreach (KeyValuePair<string, string> face_info in faces_info)
                             {
-                                faces_vertexes = face.Loops.First().Vertices;
-                            }
-                            catch (Autodesk.AutoCAD.BoundaryRepresentation.Exception ex)
-                            {
-                                doc.Editor.WriteMessage("Error " + ex);
-                            }
+                                int FaceNumber = Convert.ToInt32(face_info.Key.Split('_')[1]);
+                                Autodesk.AutoCAD.BoundaryRepresentation.Face face = solid_faces[FaceNumber];
+                                SubentityId face_id = face.SubentityPath.SubentId;
+                                var face_info_material_name = face_info.Value;
 
-                            if (faces_vertexes != null && faces_vertexes.Count() == 3)
-                            {
-                                List<Point3d> faces_points_temp = new List<Point3d>();
-
-                                foreach (Autodesk.AutoCAD.BoundaryRepresentation.Vertex OneFaceVertex in faces_vertexes)
+                                FacesProps face_data = new FacesProps();
+                                if (UseColors == true)
                                 {
-                                    faces_points_temp.Add(OneFaceVertex.Point);
+                                    face_data.color = ColorsByNames[face_info_material_name];
                                 }
-
-                                List<Point3d> faces_points = new List<Point3d>();
-                                List<string> temp_points_str = new List<string>();
-                                foreach (Point3d OnePnt in faces_points_temp)
+                                else
                                 {
-                                    string temp_point_str = GetPointStringRepresentation(OnePnt, 4);
-                                    if (!temp_points_str.Contains(temp_point_str))
-                                    {
-                                        faces_points.Add(OnePnt);
-                                        temp_points_str.Add(temp_point_str);
-                                    }
+                                    face_data.material = MaterialsByName[face_info_material_name];
                                 }
-                                if (faces_points.Count == 3)
-                                {
-                                    Point3d face_centroid = GetCentroidByFace(faces_points);
-                                    string centroid_str = GetPointStringRepresentation(face_centroid, 3);
-
-                                    var face_info_material_name = faces_info[centroid_str];
-
-                                    FacesProps face_data = new FacesProps();
-                                    if (UseColors == true)
-                                    {
-                                        //OneSolid.SetSubentityColor(face_id, ColorsByNames[face_info_material_name]);
-                                        face_data.color = ColorsByNames[face_info_material_name];
-                                    }
-                                    else
-                                    {
-                                        //ObjectId material_id = MaterialsByName[face_info_material_name];
-                                        //OneSolid.SetSubentityMaterial(face_id, material_id);
-                                        face_data.material = MaterialsByName[face_info_material_name];
-                                    }
-                                    
-                                    face_data.id = face_id;
-                                    faces.Add(face_data);
-
-                                    
-
-                                }
+                                face_data.id = face_id;
+                                faces.Add(face_data);
                             }
-                            face.Dispose();
                         }
-                        //Autodesk.Civil3D_CustomNodes.Geometry.SetAcadPointsStyle(0, 1);
-                    }
-                    if (UseColors == true)
-                    {
-                        foreach (FacesProps face_one in faces)
+                        if (UseColors == true)
                         {
-                            OneSolid.SetSubentityColor(face_one.id, face_one.color);
-                            doc.Editor.WriteMessage($"Color {face_one.color} was assigned for {face_one.id}");
+                            foreach (FacesProps face_one in faces)
+                            {
+                                OneSolid.SetSubentityColor(face_one.id, face_one.color);
+                                //doc.Editor.WriteMessage($"Color {face_one.color} was assigned for {face_one.id}");
+                            }
+                        }
+                        else
+                        {
+                            foreach (FacesProps face_one in faces)
+                            {
+                                OneSolid.SetSubentityMaterial(face_one.id, face_one.material);
+                                //doc.Editor.WriteMessage($"Material was assigned for {face_one.id}");
+                            }
                         }
                     }
-                    else
-                    {
-                        foreach (FacesProps face_one in faces)
-                        {
-                            OneSolid.SetSubentityMaterial(face_one.id, face_one.material);
-                            doc.Editor.WriteMessage($"Material was assigned for {face_one.id}");
-                        }
-                    }
+                    
 
                     tr.Commit();
                 }
